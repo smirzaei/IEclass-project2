a3734ffe5f66b5bd3cf9715976a7d854c64a35f3
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2020-06-03 17:09:47 +0430

    Accepting new connections in an async way.

--- server.py
+++ server.py
@@ -1,22 +1,39 @@
+import asyncio
 import logging
 LOG_FORMAT = "%(asctime)s - %(funcName)s:%(lineno)s [%(levelname)s] %(message)s"
 logging.basicConfig(format=LOG_FORMAT)
 logging.getLogger().setLevel(logging.INFO)
 
+from typing import Tuple, List, Any
 from socket import socket, AF_INET, SOCK_STREAM
 
 logger = logging.getLogger(__name__)
 
 PORT = 5000
 
+loop = asyncio.get_event_loop()
+
 class Server:
-    sock = socket(AF_INET, SOCK_STREAM)
+    sock: socket = socket(AF_INET, SOCK_STREAM)
+    connections: List[Tuple[socket, Any]] = []
+
+    def __init__(self) -> None:
+        self.sock.setblocking(False)
 
     def start(self, port: int):
         logger.info(f"Starting the server on port: {port}")
         
         self.sock.bind(("localhost", port))
-        self.sock.listen(1)
+        self.sock.listen(8)
+
+    async def accept_connections(self):
+        logger.info("Listening for new connections.")
+        while True:
+            connection, address = await loop.sock_accept(self.sock)
+            logger.info(f"Received a new connection from: {address}")
+
+            self.connections.append((connection, address))
+
 
     def stop(self):
         logger.info("Stopping the server.")
@@ -24,3 +41,4 @@ class Server:
 
 server = Server()
 server.start(PORT)
+loop.run_until_complete(server.accept_connections())
