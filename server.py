7148b6b9883b44c705cddc930b8ed347e62e88b9
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2020-06-03 16:50:52 +0430

    Server class + start method.

---
+++ server.py
@@ -0,0 +1,23 @@
+import logging
+LOG_FORMAT = "%(asctime)s - %(funcName)s:%(lineno)s [%(levelname)s] %(message)s"
+logging.basicConfig(format=LOG_FORMAT)
+logging.getLogger().setLevel(logging.INFO)
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+logger = logging.getLogger(__name__)
+
+PORT = 5000
+
+class Server:
+    sock = socket(AF_INET, SOCK_STREAM)
+
+    def start(self, port: int):
+        logger.info(f"Starting the server on port: {port}")
+        
+        self.sock.bind(("localhost", port))
+        self.sock.listen(1)
+
+
+server = Server()
+server.start(PORT)
